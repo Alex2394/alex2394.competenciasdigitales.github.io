c07bf2ff2a3a65b9b85cff5bd5f593ec778fc522
Alexander Molina Cardozo <alex.molina.cardozo@gmail.com>
2024-03-13 18:00:30 -0500

    Commit initial

--- js/main.js
+++ js/main.js
@@ -2,14 +2,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const buttons_start = document.querySelectorAll('.button_start');
     
-    const fila = document.querySelector('.carousel-container');
-    const carousel_items = document.querySelectorAll('.carousel-item');
-    const next = document.querySelector('.next');
-    const prev = document.querySelector('.prev');
-
     const modal_container = document.querySelector('.modal')
 
     const close_modal = document.getElementsByClassName('close')[0];
+
+    let section_active = 'N';
+
+    let fila = '';
+    let carousel_items = '';
+    let indicator_active = '';
+    let carousel_controls = '';
+    let carousel_controls_active = '';
+    let next = '';
+    let prev = '';
 
     buttons_start.forEach((button_start) => {
 
@@ -40,8 +45,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const laptop = document.querySelector('.laptop');
             const phone = document.querySelector('.phone');
+            
+            section_active = e.target.getAttribute('data-device');
 
-            if(e.target.getAttribute('data-device') === 'L'){
+            console.log(section_active);
+            
+            if(section_active === 'L'){
                 
                 phone.classList.add('ocultar')
 
@@ -51,6 +60,12 @@ document.addEventListener('DOMContentLoaded', () => {
                 laptop.classList.remove('ocultar');
                 instructions = document.querySelector('.laptop .instructions');
                 instructions.classList.add('active');
+                
+                fila = document.querySelector('.laptop .carousel-container');
+                carousel_items = document.querySelectorAll('.laptop .carousel-item');
+                next = document.querySelector('.laptop .next');
+                prev = document.querySelector('.laptop .prev');
+                
                 document.querySelector('.laptop .system-container').scrollIntoView({ behavior: 'smooth' });
             }
             else{
@@ -63,109 +78,139 @@ document.addEventListener('DOMContentLoaded', () => {
                 phone.classList.remove('ocultar');
                 instructions = document.querySelector('.phone .instructions');
                 instructions.classList.add('active');
+
+                fila = document.querySelector('.phone .carousel-container');
+                carousel_items = document.querySelectorAll('.phone .carousel-item');
+                next = document.querySelector('.phone .next');
+                prev = document.querySelector('.phone .prev');
+
                 document.querySelector('.phone .system-container').scrollIntoView({ behavior: 'smooth' });
             }
-        });
-    });
 
-    prev.addEventListener('click', () => {
-        fila.scrollLeft -= fila.offsetWidth;
-        const indicator_active = document.querySelector('.carousel-controls .active');
-        if(indicator_active.nextPreviousSibling){
-            indicator_active.nextPreviousSibling.classList.add('active');
-            indicator_active.classList.remove('active');
-        }
-    });
+            prev.addEventListener('click', () => {
+                
+                fila.scrollLeft -= fila.offsetWidth;
+                
+                if(section_active === 'L'){
+                    indicator_active = document.querySelector('.laptop .carousel-controls .active');
+                }
+                else{
+                    indicator_active = document.querySelector('.phone .carousel-controls .active');
+                }
 
-    next.addEventListener('click', () => {
-        fila.scrollLeft += fila.offsetWidth;
-        const indicator_active = document.querySelector('.carousel-controls .active');
-        if(indicator_active.nextSibling){
-            indicator_active.nextSibling.classList.add('active');
-            indicator_active.classList.remove('active');
-        }
-    });
-
-    const number_pages = Math.ceil(carousel_items.length / 5);
-
-    for(let i = 0; i < number_pages; i++){
-        
-        const indicator = document.createElement('button');
-        
-        if( i === 0 ){
-            indicator.classList.add('active');
-        }
-        
-        const carousel_controls = document.querySelector('.carousel-controls');
-        const carousel_controls_active = document.querySelector('.carousel-controls .active');
-        carousel_controls.appendChild(indicator);
-        
-        indicator.addEventListener('click', (e) => {
-            fila.scrollLeft = i * fila.offsetWidth;
-            carousel_controls_active.classList.remove('active');
-            e.target.classList.add('active');
-        });
-    }
-
-    carousel_items.forEach((carousel_item) => {
-        carousel_item.addEventListener('mouseenter', (e) => {
-            const element = e.currentTarget;
-            setTimeout(() => {
-                carousel_items.forEach(carousel_item_remove_hover => carousel_item_remove_hover.classList.remove('hover'));
-                element.classList.add('hover');
-            }, 300);
-        });
-    });
-
-    fila.addEventListener('mouseleave', (e) => {
-        setTimeout(() => {
-            carousel_items.forEach(carousel_item_remove_hover => carousel_item_remove_hover.classList.remove('hover'));
-        }, 300);
-    });
-
-    const videos = document.querySelectorAll('.carousel-item video');
-
-    videos.forEach((video) => {
-
-        video.addEventListener('click', () => {
-
-            let vid = document.createElement('video');
-
-            vid.src = video.getAttribute("src");
-            vid.autoplay = true;
-
-            let modal__content = document.querySelector('.modal__content');
-
-            modal__content.innerHTML = "";
-
-            modal__content.appendChild(vid);
-
-            const modal__content__video = document.querySelector('.modal__content video');
-
-            if(vid.src.includes('laptop')){
-                modal__content__video.classList.add('laptop');
-            }
-            else{
-                modal__content__video.classList.add('phone');
-            }
-            
-            modal_container.classList.add('open');
-
-            modal__content__video.addEventListener('ended', () => {
-                modal_container.classList.remove('open');
+                if(indicator_active.nextPreviousSibling){
+                    indicator_active.nextPreviousSibling.classList.add('active');
+                    indicator_active.classList.remove('active');
+                }
             });
 
-            document.querySelector('.modal').scrollIntoView({ behavior: 'smooth' });
+            next.addEventListener('click', () => {
+                
+                fila.scrollLeft += fila.offsetWidth;
+                
+                if(section_active === 'L'){
+                    indicator_active = document.querySelector('.laptop .carousel-controls .active');
+                }
+                else{
+                    indicator_active = document.querySelector('.phone .carousel-controls .active');
+                }
+
+                if(indicator_active.nextSibling){
+                    indicator_active.nextSibling.classList.add('active');
+                    indicator_active.classList.remove('active');
+                }
+            });
+
+            const number_pages = Math.ceil(carousel_items.length / 5);
+
+            for(let i = 0; i < number_pages; i++){
+                
+                const indicator = document.createElement('button');
+                
+                if( i === 0 ){
+                    indicator.classList.add('active');
+                }
+
+                if(section_active === 'L'){
+                    carousel_controls = document.querySelector('.laptop .carousel-controls');
+                    carousel_controls_active = document.querySelector('.laptop .carousel-controls .active');
+                }
+                else{
+                    carousel_controls = document.querySelector('.phone .carousel-controls');
+                    carousel_controls_active = document.querySelector('.phone .carousel-controls .active');
+                }
+                
+                carousel_controls.appendChild(indicator);
+                
+                indicator.addEventListener('click', (e) => {
+                    fila.scrollLeft = i * fila.offsetWidth;
+                    carousel_controls_active.classList.remove('active');
+                    e.target.classList.add('active');
+                });
+            }
+
+            carousel_items.forEach((carousel_item) => {
+                carousel_item.addEventListener('mouseenter', (e) => {
+                    const element = e.currentTarget;
+                    setTimeout(() => {
+                        carousel_items.forEach(carousel_item_remove_hover => carousel_item_remove_hover.classList.remove('hover'));
+                        element.classList.add('hover');
+                    }, 300);
+                });
+            });
+
+            fila.addEventListener('mouseleave', (e) => {
+                setTimeout(() => {
+                    carousel_items.forEach(carousel_item_remove_hover => carousel_item_remove_hover.classList.remove('hover'));
+                }, 300);
+            });
+
+            const videos = document.querySelectorAll('.carousel-item video');
+
+            videos.forEach((video) => {
+
+                video.addEventListener('click', () => {
+
+                    let vid = document.createElement('video');
+
+                    vid.src = video.getAttribute("src");
+                    vid.autoplay = true;
+
+                    let modal__content = document.querySelector('.modal__content');
+
+                    modal__content.innerHTML = "";
+
+                    modal__content.appendChild(vid);
+
+                    const modal__content__video = document.querySelector('.modal__content video');
+
+                    if(vid.src.includes('laptop')){
+                        modal__content__video.classList.add('laptop');
+                    }
+                    else{
+                        modal__content__video.classList.add('phone');
+                    }
+                    
+                    modal_container.classList.add('open');
+
+                    modal__content__video.addEventListener('ended', () => {
+                        modal_container.classList.remove('open');
+                    });
+
+                    document.querySelector('.modal').scrollIntoView({ behavior: 'smooth' });
+                });
+            });
+
+            close_modal.onclick = function() {
+                modal_container.classList.remove('open');
+            }
+
+            window.onclick = function(event) {
+                if (event.target == modal_container) {
+                    modal_container.classList.remove('open');
+                }
+            }
+
         });
     });
-
-    close_modal.onclick = function() {
-        modal_container.classList.remove('open');
-    }
-
-    window.onclick = function(event) {
-        if (event.target == modal_container) {
-            modal_container.classList.remove('open');
-        }
-    }
 });
